fc601a4d1e98cef8aec6d9de118610238a3db3f1
APR <trewto@users.noreply.github.com>
2024-12-08 04:46:05 +0600

    Assignment 4 , Part A done

--- classwork 27 nov 2024/mode_choice_mnl.R
+++ classwork 27 nov 2024/mode_choice_mnl.R
@@ -1,8 +1,3 @@
-# ################################################################# #
-#### LOAD LIBRARY AND DEFINE CORE SETTINGS                       ####
-# ################################################################# #
-#setwd("ousehold Travel Survey Data/csv/R_code")
-### Clear memory
 rm(list = ls())
 
 ### Load Apollo library
@@ -16,26 +11,16 @@ apollo_control = list(
   modelName       = "MNL_RP",
   modelDescr      = "Simple MNL model on mode choice RP data",
   indivID         = "ID", 
-  outputDirectory = "output"
+  outputDirectory = "output_RP"
 )
 
-# ################################################################# #
-#### LOAD DATA AND APPLY ANY TRANSFORMATIONS                     ####
-# ################################################################# #
 
-### Loading data from package
-### if data is to be loaded from a file (e.g. called data.csv), 
-### the code would be: database = read.csv("data.csv",header=TRUE)
-#database = apollo_modeChoiceData
 database = read.csv("apollo_modeChoiceData.csv", header = TRUE)
-### for data dictionary, use ?apollo_modeChoiceData
 
 ### Use only RP data
 database = subset(database,database$RP==1)
 
-# ################################################################# #
-#### DEFINE MODEL PARAMETERS                                     ####
-# ################################################################# #
+
 
 ### Vector of parameters, including any that are kept fixed in estimation
 apollo_beta=c(asc_car   = 0,
@@ -47,20 +32,19 @@ apollo_beta=c(asc_car   = 0,
               b_tt_air  = 0,
               b_tt_rail = 0,
               b_access  = 0,
-              b_cost    = 0)
+              b_cost_car  = 0,
+              b_cost_bus = 0 ,
+              b_cost_air = 0 ,
+              b_cost_rail = 0)
 
-### Vector with names (in quotes) of parameters to be kept fixed at their starting value in apollo_beta, use apollo_beta_fixed = c() if none
+### Vector with names (in quotes) of parameters to be kept fixed at their starting value in apollo_beta, use apollo_beta_fixed = c() if 
 apollo_fixed = c("asc_car")
+#apollo_fixed = c()
 
-# ################################################################# #
-#### GROUP AND VALIDATE INPUTS                                   ####
-# ################################################################# #
 
 apollo_inputs = apollo_validateInputs()
 
-# ################################################################# #
-#### DEFINE MODEL AND LIKELIHOOD FUNCTION                        ####
-# ################################################################# #
+
 
 apollo_probabilities=function(apollo_beta, apollo_inputs, functionality="estimate"){
   
@@ -73,10 +57,10 @@ apollo_probabilities=function(apollo_beta, apollo_inputs, functionality="estimat
   
   ### List of utilities: these must use the same names as in mnl_settings, order is irrelevant
   V = list()
-  V[["car"]]  = asc_car  + b_tt_car  * time_car                           + b_cost * cost_car
-  V[["bus"]]  = asc_bus  + b_tt_bus  * time_bus  + b_access * access_bus  + b_cost * cost_bus 
-  V[["air"]]  = asc_air  + b_tt_air  * time_air  + b_access * access_air  + b_cost * cost_air   
-  V[["rail"]] = asc_rail + b_tt_rail * time_rail + b_access * access_rail + b_cost * cost_rail  
+  V[["car"]]  = asc_car  + b_tt_car  * time_car                           + b_cost_car * cost_car
+  V[["bus"]]  = asc_bus  + b_tt_bus  * time_bus  + b_access * access_bus  + b_cost_bus * cost_bus 
+  V[["air"]]  = asc_air  + b_tt_air  * time_air  + b_access * access_air  + b_cost_air * cost_air   
+  V[["rail"]] = asc_rail + b_tt_rail * time_rail + b_access * access_rail + b_cost_rail * cost_rail  
   
   ### Define settings for MNL model component
   mnl_settings = list(
@@ -118,3 +102,38 @@ apollo_modelOutput(model)
 # ----------------------------------------------------------------- #
 
 apollo_saveOutput(model)
+
+
+
+
+# ################################################################# #
+#### VTT AND VAT CALCULATIONS                                    ####
+# ################################################################# #
+
+# Extract estimates from the model
+estimates = model$estimate
+
+# Calculate VTT and VAT
+vtt_car  = -estimates["b_tt_car"] / estimates["b_cost_car"] * 60
+vtt_bus  = -estimates["b_tt_bus"] / estimates["b_cost_bus"] * 60
+vtt_air  = -estimates["b_tt_air"] / estimates["b_cost_air"] * 60
+vtt_rail = -estimates["b_tt_rail"] / estimates["b_cost_rail"] * 60
+
+vat_car  = -estimates["b_access"] / estimates["b_cost_car"] * 60
+vat_bus  = -estimates["b_access"] / estimates["b_cost_bus"] * 60
+vat_air  = -estimates["b_access"] / estimates["b_cost_air"] * 60
+vat_rail = -estimates["b_access"] / estimates["b_cost_rail"] * 60
+
+# Create a table for VTT and VAT
+vtt_vat_table = data.frame(
+  Mode = c("Car", "Bus", "Air", "Rail"),
+  VTT_EuroPerHour = c(vtt_car, vtt_bus, vtt_air, vtt_rail),
+  VAT_EuroPerHour = c(vat_car, vat_bus, vat_air, vat_rail)
+)
+
+# Print the table
+print("Value of Travel Time (VTT) and Value of Access Time (VAT):")
+print(vtt_vat_table)
+
+# Save the table to a CSV file
+write.csv(vtt_vat_table, file = paste0(apollo_control$outputDirectory, "/vtt_vat_results.csv"), row.names = FALSE)
